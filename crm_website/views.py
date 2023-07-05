7ac2a96dfb6992ecbf44223e7c4fc1f4e1376be4
Adithya Hegde <hegdeaditya39@gmail.com>
2023-07-05 22:39:29 +0530

    individual record, deletion

--- crm_website/views.py
+++ crm_website/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # home function is a Django view that takes in the HTTP request and
 # renders the 'home.html' template and returns it as an HTTP response.
 def home(request):
+    # basically takes all the records from model.records and passes it into the webpage
+    # as a dictionary with the URL kinda ig
+    records = Record.objects.all()
 
     # Check to see if user is logging in i.e if there is a POST request from
     # the form: it has its method POST so you can access the fields in it by 
@@ -24,7 +28,8 @@ def home(request):
             messages.success(request,"There was an error logging in, Please try again")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        # if there are no records i.e user hasn't already logged in then it will render without records
+        return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
@@ -49,3 +54,25 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
         
     return render(request, 'register.html', {'form':form})
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        # Get records from our models, using the id crated by our migrations file
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view record")
+        return redirect('home')
+
+def delete_record(request,pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request,"Record deleted successfully!!!")
+        return redirect('home')
+    else:
+        messages.success(request,"You must be logged in to perform this action.")
+        return redirect('home')
+    
+def add_record(request,pk):
+    return render(request, 'record.html', {'customer_record':customer_record})
